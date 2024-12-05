5294878a4b79e5d6a80c641d76455383cf36e311
johnkimeu <johncult23@gmail.com>
2024-12-05 14:22:47 +0300

    Initial commit

---
+++ src/components/HowItWorks.jsx
@@ -0,0 +1,96 @@
+import React, { useRef } from 'react'
+import { chipImg, frameImg, frameVideo } from '../utils'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap';
+import { animateWithGsap } from '../utils/animations';
+
+const HowItWorks = () => {
+  const videoRef = useRef();
+
+  useGSAP(() => {
+    gsap.from('#chip', {
+      scrollTrigger: {
+        trigger: '#chip',
+        start: '20% bottom'
+      },
+      opacity: 0,
+      scale: 2,
+      duration: 2,
+      ease: 'power2.inOut'
+    })
+
+    animateWithGsap('.g_fadeIn', {
+      opacity: 1,
+      y: 0,
+      duration: 1,
+      ease: 'power2.inOut'
+    })
+  }, []);
+
+  return (
+    <section className="common-padding">
+      <div className="screen-max-width">
+        <div id="chip" className="flex-center w-full my-20">
+          <img src={chipImg} alt="chip" width={180} height={180} />
+        </div>
+
+        <div className="flex flex-col items-center">
+          <h2 className="hiw-title">
+            Skyward Vision.
+            <br /> An epic capture from above!
+          </h2>
+
+          <p className="hiw-subtitle">
+          Where the drone flies, creativity follows—capturing moments from angles never seen before.
+          </p>
+        </div>
+
+        <div className="mt-10 md:mt-20 mb-14">
+          <div className="relative h-full flex-center">
+            <div className="overflow-hidden">
+              <img 
+                src={frameImg}
+                alt="frame"
+                className="bg-transparent relative z-10"
+              />
+            </div>
+            <div className="hiw-video">
+                <video className="pointer-events-none" playsInline preload="none" muted autoPlay ref={videoRef}>
+                  <source src={frameVideo} type="video/mp4" />
+                </video>
+              </div>
+          </div>
+          <p className="text-gray font-semibold text-center mt-3">Kwenia: Shooting Range</p>
+          </div>
+
+          <div className="hiw-text-container">
+                <div className="flex flex-1 justify-center flex-col">
+                  <p className="hiw-text g_fadeIn">
+                  Through my lens, every moment becomes a story waiting to be told. {' '}
+                    <span className="text-white">
+                      Each shot is more than just a picture
+                    </span>.
+                  </p>
+
+                  <p className="hiw-text g_fadeIn">
+                   I don't just capture moments {' '}
+                    <span className="text-white">
+                      I freeze emotions, thoughts, and memories, preserving the essence of life as it happens.
+                    </span>
+                     with incredibly detailed environments and characters.
+                  </p>
+                </div>
+              
+
+              <div className="flex-1 flex justify-center flex-col g_fadeIn">
+                <p className="hiw-text">Change</p>
+                <p className="hiw-bigtext">Your Perspective</p>
+                <p className="hiw-text">embrace new possibilities</p>
+              </div>
+              </div>
+            </div>
+    </section>
+  )
+}
+
+export default HowItWorks  
